f8602501a82c66b8941f8b23ca316e0113210fda
Yash Patel <85803197+yash0patel1@users.noreply.github.com>
2025-03-02 13:18:20 -0500

    Update app_a.py

--- app_a.py
+++ app_a.py
@@ -4,10 +4,26 @@ from firebase_admin import credentials, firestore
 from PIL import Image
 import os
 
-# Initialize Firebase Admin SDK
-if not firebase_admin._apps:
-    cred = credentials.Certificate(r"D:\Downloads\key.json")
-    firebase_admin.initialize_app(cred)
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+cred = credentials.Certificate({
+    "type": "service_account",
+    "project_id": os.getenv("FIREBASE_PROJECT_ID"),
+    "private_key_id": os.getenv("FIREBASE_PRIVATE_KEY_ID"),
+    "private_key": os.getenv("FIREBASE_PRIVATE_KEY").replace("\\n", "\n"),
+    "client_email": os.getenv("FIREBASE_CLIENT_EMAIL"),
+    "client_id": os.getenv("FIREBASE_CLIENT_ID"),
+    "auth_uri": os.getenv("FIREBASE_AUTH_URI"),
+    "token_uri": os.getenv("FIREBASE_TOKEN_URI"),
+    "auth_provider_x509_cert_url": os.getenv("FIREBASE_AUTH_PROVIDER_X509_CERT_URL"),
+    "client_x509_cert_url": os.getenv("FIREBASE_CLIENT_X509_CERT_URL"),
+    "universe_domain": os.getenv("FIREBASE_UNIVERSE_DOMAIN")
+})
+
+firebase_admin.initialize_app(cred)
 
 db = firestore.client()
 
